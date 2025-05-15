77d115004a6bad9ceca7ce484ca9070b82bd61e8
krupinskij <jk.krupinski@gmail.com>
2025-05-15 13:09:53 +0200

    Add mockup

--- main.go
+++ main.go
@@ -16,40 +16,138 @@ type time struct {
 	Unit  string `json:"unit"`
 }
 
+type ingredient struct {
+	Name     string `json:"name"`
+	Quantity int16  `json:"quantity"`
+	Unit     string `json:"unit"`
+}
+
+type ingredients_group struct {
+	Title       string       `json:"title"`
+	Ingredients []ingredient `json:"ingredients"`
+}
+
+type methods_group struct {
+	Title   string   `json:"title"`
+	Methods []string `json:"methods"`
+}
+
 type recipe struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Image   string `json:"image"`
-	Time    time   `json:"time"`
-	Content string `json:"content"`
-	Author  user   `json:"author"`
+	ID          string              `json:"id"`
+	Title       string              `json:"title"`
+	Image       string              `json:"image"`
+	Time        time                `json:"time"`
+	Ingredients []ingredients_group `json:"ingredients"`
+	Methods     []methods_group     `json:"methods"`
+	Author      user                `json:"author"`
 }
 
 var me = user{ID: "1", Username: "krupinskij"}
 var recipes = []recipe{
 	{
-		ID:      "1",
-		Title:   "Murzynek",
-		Image:   "https://cdn.aniagotuje.com/pictures/articles/2018/03/104896-v-1000x1000.jpg",
-		Time:    time{Value: 180, Unit: "m"},
-		Content: "Upiecz murzynka",
-		Author:  me,
+		ID:    "1",
+		Title: "Murzynek",
+		Image: "https://cdn.aniagotuje.com/pictures/articles/2018/03/104896-v-1000x1000.jpg",
+		Time:  time{Value: 180, Unit: "m"},
+		Ingredients: []ingredients_group{
+			{
+				Title: "",
+				Ingredients: []ingredient{
+					{Name: "masło", Quantity: 200, Unit: "g"},
+					{Name: "ekstrakt z wanilii", Quantity: 2, Unit: "łyżeczki"},
+					{Name: "zmielone migdały", Quantity: 100, Unit: "g"},
+				},
+			},
+			{
+				Title: "polewa",
+				Ingredients: []ingredient{
+					{Name: "ciemna czekolada", Quantity: 80, Unit: "g"},
+				},
+			},
+		},
+		Methods: []methods_group{
+			{
+				Title: "",
+				Methods: []string{
+					"Ogrzać w temp. pokojowej masło, jogurt i jajka.",
+					"Piekarnik nagrzać do 160 stopni C.",
+					"Do misy miksera włożyć miękkie masło. Dodać cukier i ubijać mikserem przez ok. 5 - 7 minut, aż będzie jasne i puszyste.",
+				},
+			},
+		},
+		Author: me,
 	},
 	{
-		ID:      "2",
-		Title:   "Piernik",
-		Image:   "https://wszystkiegoslodkiego.pl/storage/images/202110/piernik-weganski.jpg",
-		Time:    time{Value: 250, Unit: "m"},
-		Content: "Upiecz piernik",
-		Author:  me,
+		ID:    "2",
+		Title: "Piernik",
+		Image: "https://wszystkiegoslodkiego.pl/storage/images/202110/piernik-weganski.jpg",
+		Time:  time{Value: 250, Unit: "m"},
+		Ingredients: []ingredients_group{
+			{
+				Title: "",
+				Ingredients: []ingredient{
+					{Name: "masło", Quantity: 125, Unit: "g"},
+					{Name: "cukier", Quantity: 250, Unit: "g"},
+					{Name: "miód", Quantity: 2, Unit: "łyżki"},
+				},
+			},
+			{
+				Title: "polewa",
+				Ingredients: []ingredient{
+					{Name: "ciemna czekolada", Quantity: 80, Unit: "g"},
+				},
+			},
+		},
+		Methods: []methods_group{
+			{
+				Title: "",
+				Methods: []string{
+					"Piekarnik nagrzać do 175 stopni C (góra i dół bez termoobiegu).",
+					"Formę keksową o wymiarach 12 x 25 cm wyłożyć papierem do pieczenia.",
+					"W garnku na małym ogniu roztopić masło, dodać cukier i wymieszać. Chwilę podgrzewać aż cukier zacznie się rozpuszczać.",
+				},
+			},
+		},
+		Author: me,
 	},
 	{
-		ID:      "3",
-		Title:   "Sernik",
-		Image:   "https://cdn.aniagotuje.com/pictures/articles/2018/11/165653-v-1000x1000.jpg",
-		Time:    time{Value: 210, Unit: "m"},
-		Content: "Upiecz sernik",
-		Author:  me,
+		ID:    "3",
+		Title: "Sernik",
+		Image: "https://cdn.aniagotuje.com/pictures/articles/2018/11/165653-v-1000x1000.jpg",
+		Time:  time{Value: 210, Unit: "m"},
+		Ingredients: []ingredients_group{
+			{
+				Title: "ciasto kruche",
+				Ingredients: []ingredient{
+					{Name: "mąka pszenna", Quantity: 350, Unit: "g"},
+				},
+			},
+			{
+				Title: "masa serowa",
+				Ingredients: []ingredient{
+					{Name: "twaróg", Quantity: 1, Unit: "kg"},
+				},
+			},
+		},
+		Methods: []methods_group{
+			{
+				Title: "ciasto kruche",
+				Methods: []string{
+					"Do mąki dodać sól, proszek do pieczenia, cukier wanilinowy, cukier oraz pokrojone w kosteczkę zimne masło.",
+					"Siekać składniki na desce lub miksować mieszadłem miksera aż powstanie drobna kruszonka. Wówczas dodać jajko i żółtka i połączyć składniki w gładkie i jednolite ciasto.",
+					"Uformować kulę, spłaszczyć ją i zawinąć w folię, włożyć do lodówki na ok. 30 minut.",
+				},
+			},
+			{
+				Title: "masa serowa",
+				Methods: []string{
+					"Połowę żółtek ubić z połową cukru pudru.",
+					"Dalej ubijając dodawać po jednym żółtku i po trochu cukru pudru.",
+					"Ser przepuścić 3 razy przez maszynkę razem z masłem lub dokładnie razem zmiksować ser i masło.",
+				},
+			},
+		},
+		Author: me,
 	},
 }
 
